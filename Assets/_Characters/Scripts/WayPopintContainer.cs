0b39084a42b82274e51555d0aff79c4e2b806f18
juan sebastian palacios <elgranpalicron@gmail.com>
2019-01-17 11:46:36 -0500

    clase 166

---
+++ Assets/_Characters/Scripts/WayPopintContainer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Character
+{
+
+	public class WayPopintContainer : MonoBehaviour
+	{
+
+
+		private void OnDrawGizmos()
+		{
+			Vector3 firts = transform.GetChild(0).position;
+			Vector3 previusPosition = firts;
+			foreach (Transform waypoint in transform)
+			{
+				Gizmos.color = Color.red;
+				Gizmos.DrawSphere(waypoint.position, .2f);
+				Gizmos.color = Color.green;
+				Gizmos.DrawLine(previusPosition, waypoint.position);
+				previusPosition = waypoint.position;
+			}
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawLine(previusPosition, firts);
+		}
+	}
+
+}
